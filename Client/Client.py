e66ad261e36a248367e3b57760e0ab0caa392732
alanolv <olvera.cristino.alan@gmail.com>
2023-09-17 13:15:10 -0600

    Se envia,encripta y desencripta la imagen

--- Client/Client.py
+++ Client/Client.py
@@ -3,18 +3,38 @@ import os
 import socket
 import struct
 def send_file(sck: socket.socket, filename):
-    # Obtener el tamaño del archivo a enviar.
     filesize = os.path.getsize(filename)
-    # Informar primero al servidor la cantidad
-    # de bytes que serán enviados.
     sck.sendall(struct.pack("<Q", filesize))
-    # Enviar el archivo en bloques de 1024 bytes.
     with open(filename, "rb") as f:
         while read_bytes := f.read(1024):
             sck.sendall(read_bytes)
+            
+def encrypt_file(filename):
+   
+    key = 107  
+    fin = open(filename, 'rb')
+
+    image = fin.read()
+    fin.close()
+        
+    image = bytearray(image)
+   
+    for index, values in enumerate(image):
+        image[index] = values ^ key
+ 
+   
+    new_filename = "encrypted_" + filename  
+    fin = open(new_filename, 'wb')
+
+    fin.write(image)
+    fin.close()
+    print('Archivo encriptado...')
+ 
+     
 with socket.create_connection(("localhost", 6190)) as conn:
     print("Conectado al servidor.")
+    encrypt_file("image.png")
     print("Enviando archivo...")
-    send_file(conn, "imagen.png")
+    send_file(conn, "encrypted_image.png")
     print("Enviado.")
 print("Conexión cerrada.")
--- Server/Server.py
+++ Server/Server.py
@@ -1,12 +1,8 @@
 # server.py
 import socket
 import struct
+
 def receive_file_size(sck: socket.socket):
-    # Esta función se asegura de que se reciban los bytes
-    # que indican el tamaño del archivo que será enviado,
-    # que es codificado por el cliente vía struct.pack(),
-    # función la cual genera una secuencia de bytes que
-    # representan el tamaño del archivo.
     fmt = "<Q"
     expected_bytes = struct.calcsize(fmt)
     received_bytes = 0
@@ -17,27 +13,53 @@ def receive_file_size(sck: socket.socket):
         received_bytes += len(chunk)
     filesize = struct.unpack(fmt, stream)[0]
     return filesize
+
 def receive_file(sck: socket.socket, filename):
-    # Leer primero del socket la cantidad de 
-    # bytes que se recibirán del archivo.
     filesize = receive_file_size(sck)
-    # Abrir un nuevo archivo en donde guardar
-    # los datos recibidos.
     with open(filename, "wb") as f:
         received_bytes = 0
-        # Recibir los datos del archivo en bloques de
-        # 1024 bytes hasta llegar a la cantidad de
-        # bytes total informada por el cliente.
         while received_bytes < filesize:
             chunk = sck.recv(1024)
             if chunk:
                 f.write(chunk)
                 received_bytes += len(chunk)
+                
+def decrypt_file(filename):
+  
+    key = 107
+     
+    fin = open(filename, 'rb')
+     
+    image = fin.read()
+    fin.close()
+
+    image = bytearray(image)
+ 
+    for index, values in enumerate(image):
+        image[index] = values ^ key
+ 
+    new_filename = "decrypted_" + filename  
+    fin = open(new_filename, 'wb')
+
+    fin.write(image)
+    fin.close()
+    
+def send_file(sck: socket.socket, filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+        sck.sendall(data)
+        
 with socket.create_server(("localhost", 6190)) as server:
     print("Esperando al cliente...")
     conn, address = server.accept()
     print(f"{address[0]}:{address[1]} conectado.")
     print("Recibiendo archivo...")
-    receive_file(conn, "imagen-recibida.png")
+    receive_file(conn, "image_receive.png")
     print("Archivo recibido.")
+    print("Desencriptando archivo")
+    decrypt_file("image_receive.png")
+    print("Archivo desencriptado")
+    print("Enviando archivo desencriptado al cliente...")
+    send_file(conn, "decrypted_image_receive.png")
+    print("Archivo desencriptado enviado al cliente.")
 print("Conexión cerrada.")
